4b724abf5b7dcdc9b0b9eebc97cd375517ecebe4
Aleksey Petrik <alexey.petric@gmail.com>
2021-05-19 15:17:33 +1000

    added functionality (save, get, delete, clear, getAll, size)

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,49 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; storage[i] != null; i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        storage[size()] = r;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; storage[i] != null; i++) {
+            if (uuid.equals(storage[i].uuid)) return storage[i];
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; storage[i] != null; i++) {
+            if (uuid.equals(storage[i].uuid)) {
+                if (i != size() - 1) {
+                    storage[i] = storage[size() - 1];
+                    storage[size() - 1] = null;
+                } else storage[i] = null;
+                break;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] resumes = new Resume[size()];
+        for (int i = 0; storage[i] != null; i++) {
+            resumes[i] = storage[i];
+        }
+        return resumes;
     }
 
     int size() {
-        return 0;
+        int counter;
+        for (counter = 0; storage[counter] != null; counter++) {
+        }
+        return counter;
     }
 }
