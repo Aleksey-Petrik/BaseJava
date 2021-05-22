84adecd65c35734471b49dada84673071e30cc03
Aleksey Petrik <alexey.petric@gmail.com>
2021-05-22 16:11:15 +1000

    remove common method in abstract class

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -7,17 +7,10 @@ import java.util.Arrays;
 /**
  * Array based storage for Resumes
  */
-public class ArrayStorage {
-    private Resume[] storage = new Resume[10_000];
-    private int size = 0;
-
-    public void clear() {
-        Arrays.fill(storage, 0, size, null);
-        size = 0;
-    }
+public class ArrayStorage extends AbstractArrayStorage {
 
     public void save(Resume r) {
-        if (size < storage.length) {
+        if (size < STORAGE_LIMIT) {
             if (findIndex(r.getUuid()) < 0) {
                 storage[size++] = r;
             } else {
@@ -26,15 +19,6 @@ public class ArrayStorage {
         } else {
             System.out.println("ERROR: Превышен лимит записей!!!");
         }
-    }
-
-    public Resume get(String uuid) {
-        int index = findIndex(uuid);
-        if (index >= 0) {
-            return storage[index];
-        }
-        System.out.println("ERROR: Резюме с id-" + uuid + " не найдено!");
-        return null;
     }
 
     public void delete(String uuid) {
@@ -52,27 +36,11 @@ public class ArrayStorage {
         }
     }
 
-    public void update(Resume r) {
-        int index = findIndex(r.getUuid());
-        if (index >= 0) {
-            storage[index] = r;
-        } else {
-            System.out.println("ERROR: Резюме с id-" + r.getUuid() + " не найдено!");
-        }
-    }
-
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    public Resume[] getAll() {
-        return Arrays.copyOfRange(storage, 0, size);
-    }
 
-    public int size() {
-        return size;
-    }
-
-    private int findIndex(String uuid) {
+    protected int findIndex(String uuid) {
         for (int i = 0; i < size; i++) {
             if (uuid.equals(storage[i].getUuid())) {
                 return i;
