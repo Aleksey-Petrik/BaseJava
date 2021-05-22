cf0624cfbdea56481604bdccd6436a6ab5b72f82
Aleksey Petrik <alexey.petric@gmail.com>
2021-05-22 15:58:02 +1000

    add odj for tests

--- src/com/urise/webapp/MainTestArrayStorage.java
+++ src/com/urise/webapp/MainTestArrayStorage.java
@@ -2,12 +2,14 @@ package com.urise.webapp;
 
 import com.urise.webapp.model.Resume;
 import com.urise.webapp.storage.ArrayStorage;
+import com.urise.webapp.storage.SortedArrayStorage;
+import com.urise.webapp.storage.Storage;
 
 /**
  * Test for your com.urise.webapp.storage.ArrayStorage implementation
  */
 public class MainTestArrayStorage {
-    static final ArrayStorage ARRAY_STORAGE = new ArrayStorage();
+    static final Storage ARRAY_STORAGE = new SortedArrayStorage();
 
     public static void main(String[] args) {
         Resume r1 = new Resume();
@@ -17,18 +19,26 @@ public class MainTestArrayStorage {
         Resume r3 = new Resume();
         r3.setUuid("uuid3");
         Resume r4 = new Resume();
-        r4.setUuid("uuid2");
+        r4.setUuid("uuid4");
         Resume r5 = new Resume();
         r5.setUuid("uuid5");
-        Resume r6 = new Resume();
-        r6.setUuid("uuid1");
 
-        ARRAY_STORAGE.save(r1);
+        Resume r6 = new Resume();
+        r6.setUuid("uuid2");
+        Resume r7 = new Resume();
+        r7.setUuid("uuid6");
+        Resume r8 = new Resume();
+        r8.setUuid("uuid1");
+
         ARRAY_STORAGE.save(r2);
         ARRAY_STORAGE.save(r3);
-        ARRAY_STORAGE.update(r4);
-        ARRAY_STORAGE.update(r5);
-        ARRAY_STORAGE.save(r6);
+        ARRAY_STORAGE.save(r1);
+        ARRAY_STORAGE.save(r5);
+        ARRAY_STORAGE.save(r4);
+
+        ARRAY_STORAGE.update(r6);
+        ARRAY_STORAGE.update(r7);
+        ARRAY_STORAGE.save(r8);
 
         System.out.println("Get r1: " + ARRAY_STORAGE.get(r1.getUuid()));
         System.out.println("Size: " + ARRAY_STORAGE.size());
