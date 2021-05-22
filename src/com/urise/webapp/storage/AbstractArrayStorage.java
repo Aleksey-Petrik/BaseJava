f9ae50ccd4dd6a2a8a7a8a269c7459a06c75d3ef
Aleksey Petrik <alexey.petric@gmail.com>
2021-05-22 16:09:09 +1000

    for Template Method, add common method

---
+++ src/com/urise/webapp/storage/AbstractArrayStorage.java
@@ -0,0 +1,49 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
+import java.util.Arrays;
+
+/**
+ * Array based storage for Resumes
+ */
+public abstract class AbstractArrayStorage implements Storage {
+    protected static final int STORAGE_LIMIT = 10_000;
+
+    protected Resume[] storage = new Resume[STORAGE_LIMIT];
+    protected int size = 0;
+
+    public void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
+    }
+
+    public Resume get(String uuid) {
+        int index = findIndex(uuid);
+        if (index >= 0) {
+            return storage[index];
+        }
+        System.out.println("ERROR: Резюме с id-" + uuid + " не найдено!");
+        return null;
+    }
+
+    public void update(Resume r) {
+        int index = findIndex(r.getUuid());
+        if (index >= 0) {
+            storage[index] = r;
+        } else {
+            System.out.println("ERROR: Резюме с id-" + r.getUuid() + " не найдено!");
+        }
+    }
+
+    public Resume[] getAll() {
+        return Arrays.copyOfRange(storage, 0, size);
+    }
+
+    public int size() {
+        return size;
+    }
+
+    protected abstract int findIndex(String uuid);
+
+}
