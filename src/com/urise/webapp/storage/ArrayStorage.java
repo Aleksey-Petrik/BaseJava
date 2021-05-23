5f60e21531fd44043861ed43d8337a41617106b4
Aleksey Petrik <alexey.petric@gmail.com>
2021-05-23 19:40:56 +1000

    remove methods save and delete in abstract class. crt deleteNull and method findIndex add function analog binarySearch

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -2,43 +2,10 @@ package com.urise.webapp.storage;
 
 import com.urise.webapp.model.Resume;
 
-import java.util.Arrays;
-
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage extends AbstractArrayStorage {
-
-    public void save(Resume r) {
-        if (size < STORAGE_LIMIT) {
-            if (findIndex(r.getUuid()) < 0) {
-                storage[size++] = r;
-            } else {
-                System.out.println("ERROR: Резюме с id-" + r.getUuid() + " уже есть в базе!");
-            }
-        } else {
-            System.out.println("ERROR: Превышен лимит записей!!!");
-        }
-    }
-
-    public void delete(String uuid) {
-        int index = findIndex(uuid);
-        if (index >= 0) {
-            size--;
-            if (index != size) {
-                storage[index] = storage[size];
-                storage[size] = null;
-            } else {
-                storage[index] = null;
-            }
-        } else {
-            System.out.println("ERROR: Резюме с id-" + uuid + " не найдено!");
-        }
-    }
-
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
 
     protected int findIndex(String uuid) {
         for (int i = 0; i < size; i++) {
@@ -46,6 +13,16 @@ public class ArrayStorage extends AbstractArrayStorage {
                 return i;
             }
         }
-        return -1;
+        return size == 0 ? -1 : (size + 1) * -1;
     }
+
+    protected void deleteNull(int index) {
+        if (index != size - 1) {
+            storage[index] = storage[size - 1];
+            storage[size - 1] = null;
+        } else {
+            storage[index] = null;
+        }
+    }
+
 }
