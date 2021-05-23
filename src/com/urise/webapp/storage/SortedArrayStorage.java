e1c649f3a718b2028e7d69a6cb376aeab30de8bb
Aleksey Petrik <alexey.petric@gmail.com>
2021-05-23 19:41:48 +1000

    remove methods save and delete in abstract class. crt deleteNull

--- src/com/urise/webapp/storage/SortedArrayStorage.java
+++ src/com/urise/webapp/storage/SortedArrayStorage.java
@@ -9,46 +9,16 @@ import java.util.Arrays;
  */
 public class SortedArrayStorage extends AbstractArrayStorage {
 
-    public void save(Resume r) {
-        if (size < STORAGE_LIMIT) {
-            int index = findIndex(r.getUuid());
-            if (index < 0) {
-                index = Math.abs(index + 1);
-                Resume currentResume = r;
-                while (currentResume != null) {
-                    Resume bufferResume = storage[index];
-                    storage[index] = currentResume;
-                    currentResume = bufferResume;
-                    index++;
-                }
-                size++;
-            } else {
-                System.out.println("ERROR: Резюме с id-" + r.getUuid() + " уже есть в базе!");
-            }
-        } else {
-            System.out.println("ERROR: Превышен лимит записей!!!");
-        }
-    }
-
-    public void delete(String uuid) {
-        int index = findIndex(uuid);
-        if (index >= 0) {
-            for (int i = index; i < size; i++) {
-                storage[i] = storage[i + 1];
-            }
-            size--;
-        } else {
-            System.out.println("ERROR: Резюме с id-" + uuid + " не найдено!");
-        }
-    }
-
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
-
     protected int findIndex(String uuid) {
         Resume searchKey = new Resume();
         searchKey.setUuid(uuid);
         return Arrays.binarySearch(storage, 0, size, searchKey);
     }
+
+    protected void deleteNull(int index) {
+        for (int i = index; i < size; i++) {
+            storage[i] = storage[i + 1];
+        }
+    }
+
 }
