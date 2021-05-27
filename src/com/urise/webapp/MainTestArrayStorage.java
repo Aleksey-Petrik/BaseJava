a77763c99df72609c16abf46156987458e8a53da
Aleksey Petrik <alexey.petric@gmail.com>
2021-05-27 21:50:22 +1000

    remove setUuid

--- src/com/urise/webapp/MainTestArrayStorage.java
+++ src/com/urise/webapp/MainTestArrayStorage.java
@@ -1,7 +1,6 @@
 package com.urise.webapp;
 
 import com.urise.webapp.model.Resume;
-import com.urise.webapp.storage.ArrayStorage;
 import com.urise.webapp.storage.SortedArrayStorage;
 import com.urise.webapp.storage.Storage;
 
@@ -12,23 +11,15 @@ public class MainTestArrayStorage {
     static final Storage ARRAY_STORAGE = new SortedArrayStorage();
 
     public static void main(String[] args) {
-        Resume r1 = new Resume();
-        r1.setUuid("uuid1");
-        Resume r2 = new Resume();
-        r2.setUuid("uuid2");
-        Resume r3 = new Resume();
-        r3.setUuid("uuid3");
-        Resume r4 = new Resume();
-        r4.setUuid("uuid4");
-        Resume r5 = new Resume();
-        r5.setUuid("uuid5");
+        Resume r1 = new Resume("uuid1");
+        Resume r2 = new Resume("uuid2");
+        Resume r3 = new Resume("uuid3");
+        Resume r4 = new Resume("uuid4");
+        Resume r5 = new Resume("uuid5");
 
-        Resume r6 = new Resume();
-        r6.setUuid("uuid2");
-        Resume r7 = new Resume();
-        r7.setUuid("uuid6");
-        Resume r8 = new Resume();
-        r8.setUuid("uuid1");
+        Resume r6 = new Resume("uuid2");
+        Resume r7 = new Resume("uuid6");
+        Resume r8 = new Resume("uuid1");
 
         ARRAY_STORAGE.save(r2);
         ARRAY_STORAGE.save(r3);
