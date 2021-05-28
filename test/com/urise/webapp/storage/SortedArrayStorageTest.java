253c1a96371d118c0294d4e2ff2d2c6c58d0136f
Aleksey Petrik <alexey.petric@gmail.com>
2021-05-28 10:07:51 +1000

    remove block @Before and init obj Storage storageTest

--- test/com/urise/webapp/storage/SortedArrayStorageTest.java
+++ test/com/urise/webapp/storage/SortedArrayStorageTest.java
@@ -1,20 +1,8 @@
 package com.urise.webapp.storage;
 
-import com.urise.webapp.model.Resume;
-import org.junit.Before;
-
 public class SortedArrayStorageTest extends AbstractArrayStorageTest {
-    private static final Storage storageTest = new SortedArrayStorage();
-
-    @Before
-    public void setUp() throws Exception {
-        storageTest.clear();
-        storageTest.save(new Resume(UUID_1));
-        storageTest.save(new Resume(UUID_3));
-        storageTest.save(new Resume(UUID_2));
-    }
 
     public SortedArrayStorageTest() {
-        super(storageTest);
+        super(new SortedArrayStorage());
     }
 }
