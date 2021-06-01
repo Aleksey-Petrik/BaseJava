23efcb90ccc025fc12ac97fee351e27a13fa3fce
Aleksey Petrik <alexey.petric@gmail.com>
2021-06-01 21:58:10 +1000

    @Override

--- src/com/urise/webapp/storage/AbstractArrayStorage.java
+++ src/com/urise/webapp/storage/AbstractArrayStorage.java
@@ -14,11 +14,13 @@ public abstract class AbstractArrayStorage extends AbstractStorage {
     protected Resume[] storage = new Resume[STORAGE_LIMIT];
     protected int size = 0;
 
+    @Override
     public void clear() {
         Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
+    @Override
     protected void saveResume(Resume r, int index) {
         if (size >= STORAGE_LIMIT) {
             throw new StorageException(r.getUuid(), "Превышен лимит записей!!!");
@@ -27,24 +29,29 @@ public abstract class AbstractArrayStorage extends AbstractStorage {
         size++;
     }
 
+    @Override
     protected Resume getResume(int index) {
         return storage[index];
     }
 
+    @Override
     protected void deleteResume(int index) {
         moveArray(index);
         storage[size - 1] = null;
         size--;
     }
 
+    @Override
     protected void updateResume(Resume r, int index) {
         storage[index] = r;
     }
 
+    @Override
     public Resume[] getAll() {
         return Arrays.copyOfRange(storage, 0, size);
     }
 
+    @Override
     public int size() {
         return size;
     }
