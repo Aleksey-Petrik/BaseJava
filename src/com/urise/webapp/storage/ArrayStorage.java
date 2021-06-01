914a098caf5a13618b1cf2350c613be182de6490
Aleksey Petrik <alexey.petric@gmail.com>
2021-06-01 22:00:37 +1000

    @Override

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -6,7 +6,7 @@ import com.urise.webapp.model.Resume;
  * Array based storage for Resumes
  */
 public class ArrayStorage extends AbstractArrayStorage {
-
+    @Override
     protected int findIndex(String uuid) {
         for (int i = 0; i < size; i++) {
             if (uuid.equals(storage[i].getUuid())) {
@@ -16,10 +16,12 @@ public class ArrayStorage extends AbstractArrayStorage {
         return -1;
     }
 
+    @Override
     protected void putResume(Resume r, int index) {
         storage[size] = r;
     }
 
+    @Override
     protected void moveArray(int index) {
         storage[index] = storage[size - 1];
     }
