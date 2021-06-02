c8bf6a45dbce177af94da14958ec0eaa7a75e04b
Aleksey Petrik <alexey.petric@gmail.com>
2021-06-03 09:31:11 +1000

    rename findIndex() -> findSearchKey()

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -7,7 +7,7 @@ import com.urise.webapp.model.Resume;
  */
 public class ArrayStorage extends AbstractArrayStorage {
     @Override
-    protected Object findIndex(String uuid) {
+    protected Object findSearchKey(String uuid) {
         for (int i = 0; i < size; i++) {
             if (uuid.equals(storage[i].getUuid())) {
                 return i;
