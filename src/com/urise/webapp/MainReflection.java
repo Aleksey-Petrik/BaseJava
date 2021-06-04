38fba196e91b1d7fc31e98cef4848875f5bd7e8a
Aleksey Petrik <alexey.petric@gmail.com>
2021-06-04 17:23:19 +1000

    add full name

--- src/com/urise/webapp/MainReflection.java
+++ src/com/urise/webapp/MainReflection.java
@@ -8,7 +8,7 @@ import java.lang.reflect.Method;
 
 public class MainReflection {
     public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
-        Resume r = new Resume();
+        Resume r = new Resume("James S. Carroll");
         Field field = r.getClass().getDeclaredFields()[0];
         Method method = r.getClass().getDeclaredMethod("toString");
         System.out.println(method.invoke(r));
