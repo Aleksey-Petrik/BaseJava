c56506fcedd00333b5be5fa9df4655c17ceb0091
Aleksey Petrik <alexey.petric@gmail.com>
2021-06-04 17:30:53 +1000

    rename MapStorageTest - MapUuidStorageTest

---
+++ test/com/urise/webapp/storage/MapUuidStorageTest.java
@@ -0,0 +1,8 @@
+package com.urise.webapp.storage;
+
+public class MapUuidStorageTest extends AbstractStorageTest {
+
+    public MapUuidStorageTest() {
+        super(new MapUuidStorage());
+    }
+}
