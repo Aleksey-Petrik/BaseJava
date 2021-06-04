79123d75c3cb01900a142685af23676c4d3da4ec
Aleksey Petrik <alexey.petric@gmail.com>
2021-06-04 17:28:08 +1000

    new module MapResumeStorageTest for tests map

---
+++ test/com/urise/webapp/storage/MapResumeStorageTest.java
@@ -0,0 +1,8 @@
+package com.urise.webapp.storage;
+
+public class MapResumeStorageTest extends AbstractStorageTest {
+
+    public MapResumeStorageTest() {
+        super(new MapResumeStorage());
+    }
+}
