4cbfabae8081bff0531e7e0559cfb95b28d28e06
Aleksey Petrik <alexey.petric@gmail.com>
2021-06-05 15:36:26 +1000

    refactoring

--- test/com/urise/webapp/storage/SuitTest.java
+++ test/com/urise/webapp/storage/SuitTest.java
@@ -4,10 +4,12 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ArrayStorageTest.class,
+@Suite.SuiteClasses({
+        ArrayStorageTest.class,
         SortedArrayStorageTest.class,
         ListStorageTest.class,
         MapUuidStorageTest.class,
-        MapResumeStorageTest.class})
+        MapResumeStorageTest.class
+})
 public class SuitTest {
 }
