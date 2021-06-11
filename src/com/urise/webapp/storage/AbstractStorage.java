b536245994ce02689ae40e7ebddcead5f3efb2d0
Aleksey Petrik <alexey.petric@gmail.com>
2021-06-11 21:36:59 +1000

    Add Logger LOG

--- src/com/urise/webapp/storage/AbstractStorage.java
+++ src/com/urise/webapp/storage/AbstractStorage.java
@@ -6,30 +6,43 @@ import com.urise.webapp.model.Resume;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.logging.*;
 
 public abstract class AbstractStorage<SK> implements Storage {
+    private final Logger LOG = Logger.getLogger(AbstractStorage.class.getName());
+    {
+        Handler handler = new ConsoleHandler();
+        handler.setFormatter(new XMLFormatter());
+        LOG.addHandler(handler);
+    }
+
     @Override
     public void save(Resume r) {
+        LOG.info("Save " + r);
         saveResume(r, getSearchKeyIfNotExist(r.getUuid()));
     }
 
     @Override
     public Resume get(String uuid) {
+        LOG.info("Get " + uuid);
         return getResume(getSearchKeyIfExist(uuid));
     }
 
     @Override
     public void delete(String uuid) {
+        LOG.info("Delete " + uuid);
         deleteResume(getSearchKeyIfExist(uuid));
     }
 
     @Override
     public void update(Resume r) {
+        LOG.info("Update " + r);
         updateResume(r, getSearchKeyIfExist(r.getUuid()));
     }
 
     @Override
     public List<Resume> getAllSorted() {
+        LOG.info("GetAllSorted");
         List<Resume> resumes = getListResume();
         Collections.sort(resumes);
         return resumes;
@@ -38,6 +51,7 @@ public abstract class AbstractStorage<SK> implements Storage {
     private SK getSearchKeyIfNotExist(String uuid) {
         SK key = findSearchKey(uuid);
         if (isExist(key)) {
+            LOG.warning(uuid + " - резюме с таким id уже есть в базе!");
             throw new ExistStorageException(uuid);
         }
         return key;
@@ -46,6 +60,7 @@ public abstract class AbstractStorage<SK> implements Storage {
     private SK getSearchKeyIfExist(String uuid) {
         SK key = findSearchKey(uuid);
         if (!isExist(key)) {
+            LOG.warning(uuid + " - резюме с таким id нет в базе!");
             throw new NotExistStorageException(uuid);
         }
         return key;
