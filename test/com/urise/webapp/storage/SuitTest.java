edd9ca93d4388afaab62ce6a226cc71f54f0dca3
Aleksey Petrik <alexey.petric@gmail.com>
2021-06-24 16:31:29 +1000

    add new classes for tests

--- test/com/urise/webapp/storage/SuitTest.java
+++ test/com/urise/webapp/storage/SuitTest.java
@@ -11,7 +11,8 @@ import org.junit.runners.Suite;
         MapUuidStorageTest.class,
         MapResumeStorageTest.class,
         ObjectStreamStorageTest.class,
-        ObjectStreamPathStorageTest.class
+        ObjectStreamPathStorageTest.class,
+        XmlPathStorageTest.class
 })
 public class SuitTest {
 }
