c580e28c7740b1b648b11af94425d0906447bea8
Aleksey Petrik <alexey.petric@gmail.com>
2021-06-24 16:29:49 +1000

    add annotations serialize xml and generation new equals and hashCode

--- src/com/urise/webapp/model/AbstractSection.java
+++ src/com/urise/webapp/model/AbstractSection.java
@@ -1,7 +1,10 @@
 package com.urise.webapp.model;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 import java.io.Serializable;
 
+@XmlAccessorType(XmlAccessType.FIELD)
 public abstract class AbstractSection implements Serializable {
     public abstract String getContents();
 }
