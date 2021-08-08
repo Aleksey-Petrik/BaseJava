045fac9d338367b5c343369e35c5f317df7b6b12
Aleksey Petrik <alexey.petric@gmail.com>
2021-08-08 14:44:04 +1000

    class SqlHelper added ExceptionUtil for convert data base error. new class ExceptionUtil for convert error data base in error app SQLException -> ExistStorageException, number error 23505.

--- src/com/urise/webapp/sql/SqlHelper.java
+++ src/com/urise/webapp/sql/SqlHelper.java
@@ -1,6 +1,5 @@
 package com.urise.webapp.sql;
 
-import com.urise.webapp.exception.ExistStorageException;
 import com.urise.webapp.exception.StorageException;
 
 import java.sql.Connection;
@@ -23,28 +22,20 @@ public class SqlHelper {
              PreparedStatement ps = connection.prepareStatement(sql)) {
             return executor.executor(ps);
         } catch (SQLException e) {
-            if ("23505".equals(e.getSQLState())) {
-                throw new ExistStorageException("Resume exists in DB!");
-            } else {
-                throw new StorageException(e);
-            }
+            throw ExceptionUtil.convertException(e);
         }
     }
 
     public <T> T transactionExecute(SqlTransaction<T> execute) {
         try (Connection connection = connectionFactory.getConnection()) {
             try {
-                connection.setAutoCommit(false);
+                connection.setAutoCommit(false);//Для выполнения нескольких операций за одну
                 T res = execute.execute(connection);
                 connection.commit();
                 return res;
             } catch (SQLException e) {
-                if ("23505".equals(e.getSQLState())) {
-                    connection.rollback();
-                    throw new ExistStorageException("Resume exists in DB!");
-                } else {
-                    throw new StorageException(e);
-                }
+                connection.rollback();//Откатываем транзакцию если ошибка
+                throw ExceptionUtil.convertException(e);
             }
         } catch (SQLException e) {
             throw new StorageException(e);
