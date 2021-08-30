cd933ae297ada4ba19399024a9824676c58bef78
Aleksey Petrik <alexey.petric@gmail.com>
2021-08-30 14:57:43 +1000

    new table section.

--- config/init_db.sql
+++ config/init_db.sql
@@ -1,3 +1,4 @@
+drop table IF EXISTS section;
 drop table IF EXISTS contact;
 drop table IF EXISTS resume;
 
@@ -9,4 +10,11 @@ create TABLE contact (id    SERIAL NOT NULL PRIMARY KEY,
                       value TEXT   NOT NULL,
                       resume_uuid CHAR(36) NOT NULL REFERENCES resume(uuid) ON delete CASCADE);
 
+CREATE TABLE section (id    SERIAL NOT NULL PRIMARY KEY,
+                      type  TEXT NOT NULL,
+                      value TEXT NOT NULL,
+                      resume_uuid CHAR(36) NOT NULL REFERENCES resume(uuid) ON DELETE CASCADE);
+
 create UNIQUE INDEX contact_uuid_type_index ON contact (resume_uuid, type);
+
+create UNIQUE INDEX section_uuid_type_index ON section (resume_uuid, type);
